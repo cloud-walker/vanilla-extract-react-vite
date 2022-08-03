8d9cf0c4e0272cd8123ebb5ab3b9938584c9cd7f
Luca Barone <baro.luc@gmail.com>
2022-08-03 23:13:33 +0200

    Add gray to theme

--- src/modules/theme/theme.css.ts
+++ src/modules/theme/theme.css.ts
@@ -1,8 +1,10 @@
-import {blue} from '@radix-ui/colors'
+import {blue, gray} from '@radix-ui/colors'
 import {createTheme} from '@vanilla-extract/css'
 
 export const [themeClass, vars] = createTheme({
   color: {
+    ...gray,
+
     ...blue,
   },
 })
